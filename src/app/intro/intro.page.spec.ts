2fbc7f905ad2a5c78d699f65b9c26f63fe377ec7
Carlos Gonzalez <carlos.gonzalezv@pca.edu.co>
2024-01-21 21:54:02 -0500

    SquidApp Primera Semana

---
+++ src/app/intro/intro.page.spec.ts
@@ -0,0 +1,13 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { IntroPage } from './intro.page';
+
+describe('IntroPage', () => {
+  let component: IntroPage;
+  let fixture: ComponentFixture<IntroPage>;
+
+
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/intro/intro.page.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.page.html',
+  styleUrls: ['./intro.page.scss'],
+})
+export class IntroPage {
+
+   slides= [
+    {
+      title: "<b>JUANES BOGOTÁ<br>SEGUNDA FECHA</b>",
+      description: "El parcero de Colombia recorrerá su país en una gira nacional por cinco ciudades luego de un año apoteósico.",
+      image: "../../assets/images/juanes.webp",
+      help_text: "<b>Lugar:</b> Movistar Arena <br><b>Ciudad:</b> Bogotá<br><b>Fecha:</b> 2024-05-23<br><b>Hora:</b> 8:00PM<br><b>Edad minima:</b> 7 años<br><b>Apertura puertas:</b> 6:00PM<br><b>Dirección:</b> Dg. 61c #26-36",
+      class: "slide-1",
+    
+    },
+    {
+      title: "<b>QUEVEDO<br>LATINOAMERIA DQE TOUR</b>",
+      description:"",
+      image: "../../assets/images/QUEVEDO.webp",
+      help_text: "<b>Lugar:</b> Movistar Arena <br><b>Ciudad:</b> Bogotá<br><b>Fecha:</b> 2024-01-20<br><b>Hora:</b> 9:00PM<br><b>Edad minima:</b> 7 años<br><b>Apertura puertas:</b> 7:00PM<br><b>Dirección:</b> Dg. 61c #26-36",
+      class: "slide-2",
+    },
+    {
+      title: "<b>ANDRES LOPEZ<br>LA PELOTA DE LETRAS</b>",
+      description:"La Pelota de Letras que ha rodado por más de 15 países está celebrando sus 20 años, y llega con todas las particularidades de nuestras generaciones a Bogotá, con un show versátil de las familias para todo el mundo donde se relata cada una de las ‘hazañas’ de nuestra vida en sociedad.",
+      image: "../../assets/images/ANDRES.webp",
+      help_text: "<b>Lugar:</b> Teatro Cafam <br><b>Ciudad:</b> Bogotá<br><b>Fecha:</b> 2024-01-26<br><b>Edad minima:</b> 14 años<br><b>Dirección:</b> Dg. 61c #26-36",
+      class: "slide-3",
+    },
+   
+
+  ]
+ 
+  constructor(
+    private router: Router) 
+    {}
+
+  goToIntro(){
+    console.log("go to home");
+    this.router.navigateByUrl('/home');
+  }
+
+
+
+}

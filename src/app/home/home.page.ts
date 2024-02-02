0e70bd11d0adf1f4b51b42687ac7b72d55420810
Carlos Gonzalez <carlos.gonzalezv@pca.edu.co>
2024-02-02 16:22:43 -0500

    Ultima semana seminario

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,19 +1,40 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Storage } from '@ionic/storage-angular';
-
+import { EventsService } from '../services/events.service';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  event_list: any;
 
   constructor(
     private router: Router,
-    private storage: Storage) 
+    private storage: Storage,
+    private events: EventsService) 
     {}
+
+    ionViewDidEnter(){
+      this.events.getEvents().then(
+        res =>{
+          this.event_list = res;
+          console.log("Eventos desde el servidor",this.event_list)
+        }
+        )
+        console.log("Local Events",this.events.getLocalEvents().events);
+
+        this.events.getCategories().then(categories => {
+          console.log("Categorías:", categories);
+         
+        })
+        const categoryId = 1;
+        this.events.getCategoryById(categoryId).then(category => {
+          console.log("Categoría por ID:", category);
+         
+        });
+      }
 
   goToIntro(){
     console.log("go to intro");
@@ -21,9 +42,5 @@ export class HomePage {
     this.storage.set('mostreLaIntro', true);
   }
 
-  ionViewDidEnter() {
-    console.log("Ya entre y vi la intro");
-    this.storage.set('yaViLaIntro', true);
-  }
 
 }
